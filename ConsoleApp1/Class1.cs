c71b5c0c474ea925deca275c00d228413e872710
warboy <dandrianov02@gmail.com>
2025-04-23 13:40:50 +0300

    1

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+         public double Calculate(string input)
+        {
+            bool good = true;
+            foreach (char c in input) 
+            {
+                if (char.IsLetter(c))
+                {
+                    good = false; break;
+                    
+                }
+            }
+            if (input is string && !String.IsNullOrEmpty(input)&&good)
+            {
+                string output = GetExpression(input);
+                double result = Counting(output);
+                result = Math.Round(result, 2, MidpointRounding.AwayFromZero);
+                return result;
+            }
+            else 
+            {
+                throw new FormatException("Неверный ввод");
+            }
+            
+        }
+        static private string GetExpression(string input)
+        {
+            string output = string.Empty; 
+            Stack<char> operStack = new Stack<char>(); 
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+                
+                if (IsDelimeter(input[i]))
+                    continue; 
+
+                
+                if (Char.IsDigit(input[i])) 
+                {
+                    
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i]; 
+                        i++; 
+
+                        if (i == input.Length) break; 
+                    }
+
+                    output += " "; 
+                    i--; 
+                }
+
+                
+                if (IsOperator(input[i])) 
+                {
+                    if (input[i] == '(') 
+                        operStack.Push(input[i]); 
+                    else if (input[i] == ')') 
+                    {
+                        
+                        char s = operStack.Pop();
+
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else 
+                    {
+                        if (operStack.Count > 0) 
+                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek())) 
+                                output += operStack.Pop().ToString() + " "; 
+
+                        operStack.Push(char.Parse(input[i].ToString())); 
+
+                    }
+                }
+            }
+
+            
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output; 
+        }
+        static private double Counting(string input)
+        {
+            double result = 0; 
+            Stack<double> temp = new Stack<double>(); 
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+                
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i])) 
+                    {
+                        a += input[i]; 
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a)); 
+                    i--;
+                }
+                else if (IsOperator(input[i])) 
+                {
+                    
+                    double a = temp.Pop();
+                    double b = temp.Pop();
+                    
+                    switch (input[i]) 
+                    {
+                        case '+': result = b + a; break;
+                        case '-': result = b - a; break;
+                        case '*': 
+                            result = b * a; 
+                            break;
+                        case '/':
+                            if (a == 0)
+                            {
+                                throw new DivideByZeroException("На ноль делить неляьзя");
+                            }
+                            else
+                            {
+                                result = b / a;
+                              
+                            }
+                                break;
+                        case '^': result = double.Parse(Math.Pow(double.Parse(b.ToString()), double.Parse(a.ToString())).ToString()); break;
+                    }
+                    
+                    temp.Push(result);
+                }
+            }
+            return temp.Peek(); 
+        }
+        static private bool IsDelimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*^()".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                case '^': return 5;
+                default: return 6;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/NUnitTestItem1.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1;
+
+public class NUnitTestItem1
+{
+    private Class1 aga;
+    [SetUp]
+    public void Setup()
+    {
+        aga = new Class1();
+    }
+
+    [Test]
+    public void SimplePlus()
+    {
+        double result = aga.Calculate("15 + 15");
+        Assert.AreEqual(30, result);
+    }
+    [Test]
+    public void SimpleMinus()
+    {
+        double result = aga.Calculate("25 - 15");
+        Assert.AreEqual(10, result);
+    }
+    [Test]
+    public void SimpleUmnozhenie()
+    {
+        double result = aga.Calculate("4 * 3");
+        Assert.AreEqual(12, result);
+    }
+    [Test]
+    public void SimpleDelenie()
+    {
+        double result = aga.Calculate("40 / 8");
+        Assert.AreEqual(5, result);
+    }
+    [Test]
+    public void HardPlus()
+    {
+        double result = aga.Calculate("187249871 + 137485");
+        Assert.AreEqual(187387356, result);
+    }
+    [Test]
+    public void HardMinus()
+    {
+        double result = aga.Calculate("1234567 - 765432");
+        Assert.AreEqual(469135, result);
+    }
+    [Test]
+    public void HardUmnozhenie()
+    {
+        double result = aga.Calculate("876543 * 478");
+        Assert.AreEqual(418987554, result,1);
+    }
+    [Test]
+    public void HardDelenie()
+    {
+        double result = aga.Calculate("123456789 / 1876");
+        Assert.AreEqual(65808.52, result);
+    }
+    [Test]
+    public void Stepen()
+    {
+        double result = aga.Calculate("2^20");
+        Assert.AreEqual(1048576, result);
+    }
+    [Test]
+    public void HardPrimerWithSkobkas()
+    {
+        double result = aga.Calculate("(234+65*34)/(23*32)");
+        Assert.AreEqual(3.32, result,0.01);
+    }
+    [Test]
+    public void simplePrimerWithSkobkas()
+    {
+        double result = aga.Calculate("(23-13)*(2+2)");
+        Assert.AreEqual(40, result);
+    }
+    [Test]
+    public void ProverkaNaCharSimbols()
+    {
+        Assert.Throws<FormatException>(() => aga.Calculate("15 + 153#"));
+        
+    }
+    [Test]
+    public void VOID()
+    {
+        Assert.Throws<FormatException>(() => aga.Calculate(""));
+    }
+    [Test]
+    public void DontVernieDannyeInInput()
+    {
+        Assert.Throws<FormatException>(() => aga.Calculate("FDSF"));
+    }
+}
